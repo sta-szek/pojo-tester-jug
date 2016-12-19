2bb978b922a1e572926324c6d9d788a98355376f
Piotr Joński <yoyo@wp.eu>
2016-12-19 19:03:16 +0100

    exclude field

--- src/test/java/pl/pojo/tester/model/Pojo2Test.java
+++ src/test/java/pl/pojo/tester/model/Pojo2Test.java
@@ -3,13 +3,14 @@ package pl.pojo.tester.model;
 import org.junit.Test;
 import pl.pojo.tester.api.ClassAndFieldPredicatePair;
 
+import static pl.pojo.tester.api.FieldPredicate.exclude;
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 
 public class Pojo2Test {
 
     @Test
     public void testPojo2() {
-        final ClassAndFieldPredicatePair baseClass = new ClassAndFieldPredicatePair(Pojo2.class);
+        final ClassAndFieldPredicatePair baseClass = new ClassAndFieldPredicatePair(Pojo2.class, exclude("pojo"));
         final ClassAndFieldPredicatePair compositeClass = new ClassAndFieldPredicatePair(Pojo.class);
         assertPojoMethodsFor(baseClass, compositeClass).areWellImplemented();
     }
