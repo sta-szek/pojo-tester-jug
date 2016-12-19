82486efe0ecaf477e5e8542013a71b69e9f195a1
Piotr Joński <yoyo@wp.eu>
2016-12-19 19:15:36 +0100

    bulk tests

--- src/main/java/pl/pojo/tester/model/Pojo2.java
+++ src/main/java/pl/pojo/tester/model/Pojo2.java
@@ -1,10 +1,12 @@
 package pl.pojo.tester.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Pojo2 {
 
