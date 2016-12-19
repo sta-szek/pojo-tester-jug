4b2666664afc94c6d5d0b6af88daf15e26ea57f3
Piotr Joński <yoyo@wp.eu>
2016-12-19 19:11:54 +0100

    test using create(...)

--- src/test/java/pl/pojo/tester/model/Pojo2Test.java
+++ src/test/java/pl/pojo/tester/model/Pojo2Test.java
@@ -2,6 +2,7 @@ package pl.pojo.tester.model;
 
 import org.junit.Test;
 import pl.pojo.tester.api.ClassAndFieldPredicatePair;
+import pl.pojo.tester.api.ConstructorParameters;
 
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 
@@ -14,7 +15,9 @@ public class Pojo2Test {
                 new ClassAndFieldPredicatePair(Pojo.class),
                 new ClassAndFieldPredicatePair(Pojo3.class)
         };
+        final ConstructorParameters constructorParameters = new ConstructorParameters(new Object[]{123}, new Class[]{int.class});
 
-        assertPojoMethodsFor(baseClass, compositeClasses).areWellImplemented();
+        assertPojoMethodsFor(baseClass, compositeClasses).create(Pojo3.class, constructorParameters)
+                                                         .areWellImplemented();
     }
 }
